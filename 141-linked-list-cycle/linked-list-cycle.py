13a96ce926e2d03a6caa1541387487a0bfa52825
abhinavbansal1910 <abhinavbansal1910@gmail.com>
2026-07-04 16:36:48 +0530

    Time: 45 ms (93.80%) | Memory: 22.5 MB (85.69%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -3,14 +3,11 @@ class Solution:
         # Initialize the two pointers
         slow, fast = head, head
 
-        # Traverse the list with the two pointers
-        while fast is not None and fast.next is not None:
-            slow = slow.next  # Move the slow pointer one step
-            fast = fast.next.next  # Move the fast pointer two steps
-
-            # If there's a cycle, the two pointers will meet
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next 
+            
             if slow == fast:
                 return True
-
-        # If the fast pointer reaches the end, there's no cycle
         return False
+        
